504c29f92236b10eda96942bdec8a07867a555f9
Edison Adrade <eandradep@est.ups.edu.ec>
2017-12-04 19:20:55 +0000

    Usuario.java editado a través de la web en Bitbucket

--- src/main/java/acg/pug/uega/freekick/Modelo/Usuario.java
+++ src/main/java/acg/pug/uega/freekick/Modelo/Usuario.java
@@ -1,56 +1,5 @@
 package acg.pug.uega.freekick.Modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 public class Usuario 
 {
-	@Id
-	private int ID;
-	private String Nombre;
-	private String Apellido;
-	private String Correo;
-	private String Contraseña;
-	public int getID() {
-		return ID;
-	}
-	public void setID(int iD) {
-		ID = iD;
-	}
-	public String getNombre() {
-		return Nombre;
-	}
-	public void setNombre(String nombre) {
-		Nombre = nombre;
-	}
-	public String getApellido() {
-		return Apellido;
-	}
-	public void setApellido(String apellido) {
-		Apellido = apellido;
-	}
-	public String getCorreo() {
-		return Correo;
-	}
-	public void setCorreo(String correo) {
-		Correo = correo;
-	}
-	public String getContraseña() {
-		return Contraseña;
-	}
-	public void setContraseña(String contraseña) {
-		Contraseña = contraseña;
-	}
-	
-	public String probar() {
-		
-		
-		return null;
-	}
-	
-	
-	
-	
-
 }
