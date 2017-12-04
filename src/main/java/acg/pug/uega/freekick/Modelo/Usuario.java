8e4c62b911deca892bfba6c751453a6e3f8d446c
edisonandrade <edisonandrade@172.16.214.76>
2017-12-04 13:06:35 -0500

    Creacion de Set y Get

--- src/main/java/acg/pug/uega/freekick/Modelo/Usuario.java
+++ src/main/java/acg/pug/uega/freekick/Modelo/Usuario.java
@@ -8,6 +8,44 @@ public class Usuario
 	private String Direccion;
 	private String Correo;
 	private String Contraseña;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	public String getCorreo() {
+		return Correo;
+	}
+	public void setCorreo(String correo) {
+		Correo = correo;
+	}
+	public String getContraseña() {
+		return Contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		Contraseña = contraseña;
+	}
+	
+	
 	
 	
 
