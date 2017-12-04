9c80f0496aac90ccc707d36b9d886a7576aea0b7
edisonandrade <edisonandrade@172.16.214.76>
2017-12-04 12:45:37 -0500

    Configuracion Usuario

--- src/main/java/acg/pug/uega/freekick/Modelo/Usuario.java
+++ src/main/java/acg/pug/uega/freekick/Modelo/Usuario.java
@@ -2,6 +2,13 @@ package acg.pug.uega.freekick.Modelo;
 
 public class Usuario 
 {
-	private int 
+	private int ID;
+	private String Nombre;
+	private String Apellido;
+	private String Direccion;
+	private String Correo;
+	private String Contraseña;
+	
+	
 
 }
